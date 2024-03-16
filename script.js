34fa98ff6bcaf4ff6973338b2bf4a75a53435a43
slimesarecool <arinmanchannel@gmail.com>
2024-03-15 20:57:05 -0700

    more work on confetti

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ let dat = {
 
 let entries = document.querySelectorAll(".entry")
 
+for (let i = 0; i < 10; i++) {
+    let tmpcon = document.createElement("div")
+    tmpcon.classList = "confetti"
+
+    document.body.appendChild(tmpcon)
+
+    getComputedStyle(tmpcon).setProperty("--pos", rand(0, 100).toString() + "vw")
+}
+
 for (let i = 0; i < entries.length; i++) {
     entries[i].onclick = () => {
         if (Object.values(entries[i].classList).indexOf('unchecked') > -1) {
@@ -25,5 +34,9 @@ function save(data) {
 function load(name) {
     return localStorage.getItem(name)
 }
+
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}  
 
 // document.addEventListener("contextmenu", e => e.preventDefault())
